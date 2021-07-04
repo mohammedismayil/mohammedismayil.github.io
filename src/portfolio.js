e42f33dd061bb7f86adbeb576a23680f65bf07b5
mohammedismayil <mohammedismayil10@gmail.com>
2021-07-04 08:43:47 +0530

    edited some linezs

--- src/portfolio.js
+++ src/portfolio.js
@@ -120,10 +120,10 @@ https://fontawesome.com/icons?d=gallery */
     //   skillName: "python",
     //   fontAwesomeClassname: "fab fa-python"
     // },
-    // {
-    //   skillName: "docker",
-    //   fontAwesomeClassname: "fab fa-docker"
-    // }
+    {
+      skillName: "docker",
+      fontAwesomeClassname: "fab fa-docker"
+    }
   ],
   display: true // Set false to hide this section, defaults to true
 };
