4e78130b210eb8563fef938795e3a2c267fce824
ismayil <mohammedismayil10@gmail.com>
2021-11-02 08:33:15 +0530

    server side folder created

--- server/index.js
+++ server/index.js
@@ -5,7 +5,10 @@ const port = 4000;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+app.get("/hello", (req, res) => {
+  res.send("Hello hello route !");
+  console.log("i am from express server");
+});
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
